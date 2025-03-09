758461301fe1994826bffe5efec6939f607e239c
Marcos <rijuma@users.noreply.github.com>
2025-03-09 13:55:21 -0300

    Added scss, pico and msdvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,25 @@
+import { mdsvex } from 'mdsvex'
 import adapter from '@sveltejs/adapter-node'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
+/** @type {import('mdsvex').msdvexOptions} */
+const msdvexOptions = {
+  extensions: ['md'],
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess(), mdsvex(msdvexOptions)],
 
   kit: {
     adapter: adapter(),
+    alias: {
+      $lib: './src/lib',
+      $src: './src',
+    },
   },
+
+  extensions: ['.svelte', '.md'],
 }
 
 export default config
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
+import { readFileSync } from 'node:fs'
+import { fileURLToPath } from 'url'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
+// Add package version to the env vars
+const file = fileURLToPath(new URL('package.json', import.meta.url))
+const json = readFileSync(file, 'utf8')
+const pkg = JSON.parse(json)
+process.env.PUBLIC_APP_VERSION = pkg.version
+
 export default defineConfig({
   plugins: [sveltekit()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        additionalData: `
+          @use '$src/styles/_preprocess/index' as *;
+        `,
+      },
+    },
+  },
 })
