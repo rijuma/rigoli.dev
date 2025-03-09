3175682f67275bd712054c1d6252c77b3d55b54a
Marcos <rijuma@users.noreply.github.com>
2025-03-09 14:15:02 -0300

    Added layout and main footer

---
+++ src/lib/components/site-footer.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  const year = new Date().getFullYear()
+</script>
+
+<footer class="footer">
+  &copy; {year} J. M. Rigoli
+</footer>
+
+<style lang="scss">
+  footer {
+    font-size: 0.6rem;
+    text-align: center;
+    padding: 0.2rem 0.4rem;
+  }
+</style>
---
+++ src/routes/(home)/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { SiteFooter } from '$lib/components'
+
+  let { children } = $props()
+</script>
+
+<div class="layout">
+  <main>
+    {@render children()}
+  </main>
+  <SiteFooter />
+</div>
+
+<style lang="scss">
+  .layout {
+    min-height: 100dvh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  main {
+    flex-grow: 1;
+  }
+</style>
